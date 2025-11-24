cf74557b8f82a8d600fca7ce6932a4846b8f8189
ThangHauiK16 <nguyenhoangthang26032003@gmail.com>
2025-11-24 15:18:53 +0700

    .

--- JWT-Project-Core/obj/Debug/net8.0/JWT-Project-Core.AssemblyInfo.cs
+++ JWT-Project-Core/obj/Debug/net8.0/JWT-Project-Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JWT-Project-Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9213f09e5e74f57350f9ab778f8ebfd5e47198")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84177460ed03c7b6c2f6812b0917b16f88042c10")]
 [assembly: System.Reflection.AssemblyProductAttribute("JWT-Project-Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JWT-Project-Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
